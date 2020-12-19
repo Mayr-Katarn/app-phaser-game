775d942d4bb9fb5b1abb1e31ebb7f5dd96f0aadf
Mayr-Katarn <redtein77@live.com>
2020-12-19 23:26:27 +0700

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+    publicPath: '/'
+  }
+};
